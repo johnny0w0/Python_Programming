e1de7da05f9537a0092d9420f79f5232161028a3
johnny0w0 <268320293+johnny0w0@users.noreply.github.com>
2026-09-15 09:50:57 +0900

    add ex5.py ex6.py

--- 04_collection/ex6.py
+++ 04_collection/ex6.py
@@ -8,6 +8,14 @@ d = {"name": "뽀로로", "age": 5}
 
 d2 = {"age": 23, "city": "일산"}
 
+d.update(d2)  # upsert
+print(d)
+
+print(d.pop("city"))
+print(d)
+
+d.clear()
+print(d)
 
 # ===========================================================
 #  그 외
@@ -16,13 +24,25 @@ d2 = {"age": 23, "city": "일산"}
 d = {"kor": 90, "mat": 85, "eng": 80, "prog": 100}
 
 # 딕셔너리 언패킹
+print(*d)
 
+a, *b, c = d
+print(a, b, c)
+
+print({**d})
+
+d2 = {"eng": 90, "sci": 95}
+print({**d, **d2})  # update 동일하게 동작
 
 # 위 딕셔너리를 key 리스트와 value 리스트로 만들기
-
+print(list(d))
+print(list(d.values()))
 
 # 리스트를 다시 딕셔너리로 만들기
 # zip 함수로 튜플을 먼저 만들고, dict()에서 튜플의 [0]값을 key로, [1]값을 value로 넣음
+subjects = list(d)
+scores = list(d.values())
+print(dict(zip(subjects, scores)))
 
 
 # ===========================================================
@@ -31,18 +51,23 @@ d = {"kor": 90, "mat": 85, "eng": 80, "prog": 100}
 
 # 1 ~ 10의 제곱수 딕셔너리 만들기
 # {1: 1, 2: 4, 3: 9, 4: 16, 5: 25 .. }
-
+result = {i: i**2 for i in range(1, 11)}
+print(result)
 
 # 1 ~ 10 중 홀수 제곱수로 된 딕셔너리 만들기
 # {1: 1, 3: 9, 5: 25, 7: 49, 9: 81}
-
+result = {i: i**2 for i in range(1, 11, 2)}
+print(result)
 
 # 위 result 딕셔너리의 키를 값으로, 값을 키로 바꾸기
 # {1: 1, 9: 3, 25: 5, 49: 7, 81: 9}
-
+result = {v: k for k, v in result.items()}
+print(result)
 
 # 점수 90 이상만 필터링하기
-
+scores = {"국어": 90, "영어": 85, "수학": 95, "과학": 89}
+result = {k: v for k, v in scores.items() if v >= 90}
+print(result)
 
 # =========================================================
 #  🔥 실습 문제
@@ -51,6 +76,10 @@ d = {"kor": 90, "mat": 85, "eng": 80, "prog": 100}
 # 1️⃣ 바구니에 있는 과일의 단어 개수 세기
 words = ["apple", "banana", "apple", "cherry", "banana", "apple"]
 
+count = {}
+for word in words:
+    count[word] = count.get(word, 0) + 1
+print(count)
 
 # ✅ {'apple': 3, 'banana': 2, 'cherry': 1}
 
@@ -58,6 +87,8 @@ words = ["apple", "banana", "apple", "cherry", "banana", "apple"]
 # 2️⃣ 60점 이상인 경우 합격 설정하기
 scores = {"국어": 85, "영어": 50, "수학": 95, "과학": 40, "사회": 72}
 
+result = {k: "합격" for k, v in scores.items() if v >= 60}
+print(result)
 
 # ✅ {'국어': '합격', '수학': '합격', '사회': '합격'}
 
@@ -66,6 +97,9 @@ scores = {"국어": 85, "영어": 50, "수학": 95, "과학": 40, "사회": 72}
 subjects = ["국어", "영어", "수학"]
 grades = [90, 80, 100]
 
+print(dict(zip(subjects, grades)))
+# result = {subjects[i]: grades[i] for i in range(0, len(subjects))}
+# print(result)
 
 # ✅ {'국어': 90, '영어': 80, '수학': 100}
 
@@ -74,5 +108,9 @@ grades = [90, 80, 100]
 stock = {"연필": 10, "지우개": 5, "노트": 3}  # 기존 재고
 incoming = {"지우개": 4, "노트": 7, "볼펜": 12}  # 입고 내역
 
+for item, qty in incoming.items():
+    stock[item] = stock.get(item, 0) + qty
+
+print(stock)
 
 # ✅ {'연필': 10, '지우개': 9, '노트': 10, '볼펜': 12}
